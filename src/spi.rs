b78f0ee0a97a1cf3377b8f4d686b0fc42663296d
Louis Thiery <louis@helium.com>
2019-05-08 10:14:25 -0700

    spi pins

--- src/spi.rs
+++ src/spi.rs
@@ -1,7 +1,9 @@
 #[cfg(feature = "stm32l0x1")]
 use crate::gpio::gpioa::{PA5, PA6, PA7};
 #[cfg(feature = "stm32l0x2")]
-use crate::gpio::gpioa::{PB3, PA6, PA7};
+use crate::gpio::gpioa::PA6, PA7};
+#[cfg(feature = "stm32l0x2")]
+use crate::gpio::gpiob::PB3;
 
 use crate::gpio::{Floating, Input};
 use crate::hal;
