e49014168d4af4bd66e8a9e3ce89cd8b897eac26
Louis Thiery <louis@helium.com>
2019-05-22 10:02:30 -0700

    add port C. increment version

--- src/gpio.rs
+++ src/gpio.rs
@@ -438,7 +438,6 @@ macro_rules! gpio {
     }
 }
 
-//#[cfg(any(feature = "stm32l011"))]
 gpio!(GPIOA, gpioa, iopaen, PA, [
     PA0: (pa0, 0, Input<Floating>),
     PA1: (pa1, 1, Input<Floating>),
@@ -458,7 +457,6 @@ gpio!(GPIOA, gpioa, iopaen, PA, [
     PA15: (pa15, 15, Input<Floating>),
 ]);
 
-//#[cfg(any(feature = "stm32l011"))]
 gpio!(GPIOB, gpiob, iopben, PB, [
     PB0: (pb0, 0, Input<Floating>),
     PB1: (pb1, 1, Input<Floating>),
@@ -476,4 +474,24 @@ gpio!(GPIOB, gpiob, iopben, PB, [
     PB13: (pb13, 13, Input<Floating>),
     PB14: (pb14, 14, Input<Floating>),
     PB15: (pb15, 15, Input<Floating>),
+]);
+
+#[cfg(any(feature = "stm32l0x2"))]
+gpio!(GPIOC, gpioc, iopben, PC, [
+    PC0: (pc0, 0, Input<Floating>),
+    PC1: (pc1, 1, Input<Floating>),
+    PC2: (pc2, 2, Input<Floating>),
+    PC3: (pc3, 3, Input<Floating>),
+    PC4: (pc4, 4, Input<Floating>),
+    PC5: (pc5, 5, Input<Floating>),
+    PC6: (pc6, 6, Input<Floating>),
+    PC7: (pc7, 7, Input<Floating>),
+    PC8: (pc8, 8, Input<Floating>),
+    PC9: (pc9, 9, Input<Floating>),
+    PC10: (pc10, 10, Input<Floating>),
+    PC11: (pc11, 11, Input<Floating>),
+    PC12: (pc12, 12, Input<Floating>),
+    PC13: (pc13, 13, Input<Floating>),
+    PC14: (pc14, 14, Input<Floating>),
+    PC15: (pc15, 15, Input<Floating>),
 ]);
